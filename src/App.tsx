f4250fee42c5d0dc7a8a38842330c3b86c2f6e49
lightworkerPiyo <108678829+lightworkerPiyo@users.noreply.github.com>
2022-07-08 16:27:42 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,46 @@
+import axios from "axios";
+import { useState } from "react";
+import "./styles.css";
+import { Todo } from "./Todo";
+import { TodoType } from "./types/todo";
+import { Text } from "./Text";
+import { UserProfile } from "./UserProfile";
+import { User } from "./types/user";
+
+const user: User = {
+  name: "からす",
+  hobbies: ["飛ぶ", "鳴く"]
+};
+
+// type TodoType = {
+//   userId: number;
+//   id: number;
+//   title: string;
+//   completed: boolean;
+// };
+export default function App() {
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
+
+  const onClickGet = () => {
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
+  };
+
+  return (
+    <div className="App">
+      <UserProfile user={user} />
+      <Text color="red" fontSize="18px" />
+      <button onClick={onClickGet}>ダウンロード</button>
+      {todos.map((todo) => (
+        <Todo
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed}
+        />
+      ))}
+    </div>
+  );
+}
